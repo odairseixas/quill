19cdd0951347538aab4980305f0d0fdb5b641d1b
Odair Seixas Jr <odairseixas@gmail.com>
2018-11-10 10:53:30 -0300

    Export default

--- formats/weight.js
+++ formats/weight.js
@@ -5,4 +5,4 @@ const WeightStyle = new StyleAttributor('weight', 'font-weight', {
   whitelist: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
-export { WeightStyle };
+export default WeightStyle;
